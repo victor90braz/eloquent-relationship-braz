1e3b73dc3f6aee0480265bd731736b167c3f69d1
victor braz <victor.90braz@gmail.com>
2023-11-02 20:48:10 +0100

    App\Models\Post::factory(5)->create()

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,13 +21,16 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'experience_points' => 0,
+            'username' => $this->faker->unique()->userName(), // Generate unique usernames
         ];
     }
+
 
     /**
      * Indicate that the model's email address should be unverified.
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,16 +13,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
+            $table->string('username')->unique()->nullable(); // Add 'nullable'
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->unsignedInteger('experience_points')->default(0);
+            $table->unsignedInteger('experience_points');
             $table->rememberToken();
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
