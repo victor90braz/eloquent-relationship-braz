79d79bb7678c8a254573016f49d553df97c4eff2
victor braz <victor.90braz@gmail.com>
2023-11-03 10:07:11 +0100

    foreign('tag_id')

--- database/migrations/2023_11_03_073148_create_post_tag_table.php
+++ database/migrations/2023_11_03_073148_create_post_tag_table.php
@@ -12,9 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('post_tag', function (Blueprint $table) {
+
             $table->primary(['post_id', 'tag_id']);
+
             $table->unsignedInteger('post_id');
             $table->unsignedInteger('tag_id');
+
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
