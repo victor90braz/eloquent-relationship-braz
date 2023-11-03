1a98a33b91ab095fa5ea470cd01c94dd99b23092
victor braz <victor.90braz@gmail.com>
2023-11-03 16:47:56 +0100

    $this->belongsTo(Series::class)

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Video extends Model
 {
     use HasFactory;
+
+    public function series()
+    {
+        return $this->belongsTo(Series::class);
+    }
 }
