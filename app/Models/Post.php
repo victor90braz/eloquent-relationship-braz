37f941485594e66793f62c4d033a9129071943c0
victor braz <victor.90braz@gmail.com>
2023-11-03 14:36:30 +0100

    $this->belongsToMany(Tag::class, 'relationships')->withTimestamps();

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,7 +16,7 @@ class Post extends Model
 
     public function tags ()
     {
-        return $this->belongsToMany(Tag::class)->withTimestamps();
+        return $this->belongsToMany(Tag::class, 'relationships')->withTimestamps();
     }
 }
 
