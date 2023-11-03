090ab3ecd3d17784c9f6ccc524027d8dc13edebf
victor braz <victor.90braz@gmail.com>
2023-11-03 08:22:39 +0100

    create relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public function comments ()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function tags ()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
