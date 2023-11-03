c55a0f53b32b4d78c75196758c27c1a61be51bab
victor braz <victor.90braz@gmail.com>
2023-11-03 17:15:11 +0100

    collection

--- app/Models/Collection.php
+++ app/Models/Collection.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Collection extends Model
 {
     use HasFactory;
+
+    public function videos()
+    {
+        return $this->morphMany(Video::class, 'watchable');
+    }
 }
