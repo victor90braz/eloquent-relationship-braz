3f459fa0b38ede65752ce9cb9066d16cbd58a60b
victor braz <victor.90braz@gmail.com>
2023-11-03 09:45:33 +0100

    ->withTimestamps()

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,7 +16,7 @@ class Post extends Model
 
     public function tags ()
     {
-        return $this->belongsToMany(Tag::class);
+        return $this->belongsToMany(Tag::class)->withTimestamps();
     }
 }
 
