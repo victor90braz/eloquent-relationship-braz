33c582f2ffdd7bc6db12365ac1ca45b2d388b245
victor braz <victor.90braz@gmail.com>
2023-11-03 16:51:12 +0100

    collection schema and         $table->morphs

--- database/migrations/2023_11_03_154818_create_collections_table.php
+++ database/migrations/2023_11_03_154818_create_collections_table.php
@@ -11,12 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('videos', function (Blueprint $table) {
+        Schema::create('collections', function (Blueprint $table) {
             $table->id();
-            $table->morphs('watchable');
-            $table->string('title');
-            $table->text('description');
-            $table->string('url');
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -26,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('videos');
+        Schema::dropIfExists('collections');
     }
 };
