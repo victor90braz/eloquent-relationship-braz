27099675aec7ce49f346e3a429a2973860955c2a
victor braz <victor.90braz@gmail.com>
2023-11-04 10:03:30 +0100

    fix bug

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $user = App\Models\User::first();
-
-    $post = $user->posts()->create([
-        'title' => 'foobar',
-        'body' => 'loren ipsum'
-    ]);
-
-    $post->tags()->attach(1);
-
     return view('welcome');
 });
